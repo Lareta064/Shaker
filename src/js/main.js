7f78737c45b1522d8ab6d7245f90d9b6e1ea9c69
Lareta064 <lareta064@gmail.com>
2024-05-15 17:15:17 +0300

    mobile header

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-	
+	/* ======  menu icon click ====== */
+	const menuToggle = document.querySelector('#menu-toggle');
+	const mobileMenu = document.querySelector('#header-menu');
+	const bodyEl = document.body;
+
+	if (menuToggle) {
+
+		/*   клик поиконке гамбургер*/  
+		menuToggle.addEventListener('click', ()=> {
+			
+			if (menuToggle.classList.contains('active')) {
+
+				menuToggle.classList.remove('active');
+				mobileMenu.classList.remove('active');
+				bodyEl.classList.remove('lock');
+			
+			} else {
+				menuToggle.classList.add('active');
+			    mobileMenu.classList.add('active');
+				bodyEl.classList.add('lock');
+			}
+		});
+
+       /*   клик по мобильному меню*/  
+		mobileMenu.addEventListener('click', () => {
+			menuToggle.classList.remove('active');
+			mobileMenu.classList.remove('active');
+			bodyEl.classList.remove('lock');
+		});
+	}
 
 });
